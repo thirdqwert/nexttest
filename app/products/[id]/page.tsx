5e9c5563e5ed3bab7f09dd3ccedde249956dfd78
qwert <qwert@gmail.com>
2024-11-13 14:07:26 +0500

    first

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = ({ params }: any) => {
+    return (
+        <div>
+            Product ID :{params.id}
+        </div>
+    )
+}
+
+export default Product
